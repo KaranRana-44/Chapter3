1205b820117c7b2668dcc4c9ad91c4ac4958b93a
karan <karanrana@billericak12.com>
2021-10-31 20:48:18 -0400

    HW changes

--- src/com/company/DualSpinner.java
+++ src/com/company/DualSpinner.java
@@ -1,29 +1,50 @@
 package com.company;
 public class DualSpinner {
-    /** Precondition: min < max
-     */
-    /*The spin method simulates a spin of a fair spinner.
-     * The method returns a random integer between min and max,
-     * inclusive. Complete the spin method below by assigning
-     * this random integer to result.
-     */
     public int spin(int min, int max)
     {
-        int result=0;
-//add code here
-        return result;
+        return (int)((Math.random() * (max - min + 1))+ min);
     }
 
-    /** Simulates one round of the game as described in part (b).
-     */
     public void playRound()
     {
-// add code here
+        int player = this.spin(1,10);
+        int computer = this.spin(2,8);
+        System.out.println(player);
+        System.out.println(computer);
+        int pReroll= player+spin(1,10);
+        int cReroll= computer+spin(2,8);
+
+        if (player == computer){
+            if (pReroll == cReroll) {
+                System.out.println(pReroll - player);
+                System.out.println(cReroll - computer);
+                System.out.println("Tie. 0 points.");
+            }
+            else if (pReroll>cReroll){
+                System.out.println(pReroll-player);
+                System.out.println(cReroll-computer);
+                System.out.println("You win! 1 points");
+            }
+
+            else{
+                System.out.println(pReroll-player);
+                System.out.println(cReroll-computer);
+                System.out.println("You lose! -1 point.");
+            }
+
+        }
+        else if (player > computer){
+            System.out.println("You win! "+(player-computer)+" points.");
+        }
+        else {
+            System.out.println("You lose! "+(player-computer)+" points.");
+        }
     }
     public static void main(String[] args) {
         DualSpinner ds = new DualSpinner();
         for(int i = 0; i < 10; i++){
             ds.playRound();
         }
+
     }
 }
